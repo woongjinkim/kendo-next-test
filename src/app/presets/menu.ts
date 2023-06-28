62086331e125043de2467d1ef0511daf54ba431a
wj.kim <wj.kim@zipida.com>
2023-06-28 16:51:48 +0900

    Add header

---
+++ src/app/presets/menu.ts
@@ -0,0 +1,14 @@
+const menu = [
+  {
+    label: "대시보드",
+    url: "/dashboard",
+  },
+  {
+    label: "공지사항",
+    url: "/board",
+  },
+];
+
+export default function useMenu() {
+  return menu;
+}
---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  AppBar,
+  AppBarSection,
+  AppBarSpacer,
+  Avatar,
+} from "@progress/kendo-react-layout";
+import { Badge, BadgeContainer } from "@progress/kendo-react-indicators";
+import useMenu from "@/app/presets/menu";
+import { useRouter } from "next/navigation";
+
+interface LayoutProp {
+  children: React.ReactNode;
+}
+
+let kendokaAvatar =
+  "https://www.telerik.com/kendo-react-ui-develop/components/images/kendoka-react.png";
+
+export default function MainLayout({ children }: LayoutProp) {
+  const router = useRouter();
+  const menu = useMenu();
+  return (
+    <>
+      <AppBar>
+        <AppBarSection>
+          <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base">
+            <span className="k-icon k-i-menu" />
+          </button>
+        </AppBarSection>
+
+        <AppBarSpacer style={{ width: 4 }} />
+
+        <AppBarSection>
+          <h1 className="title">TestProject</h1>
+        </AppBarSection>
+
+        <AppBarSpacer style={{ width: 32 }} />
+
+        <AppBarSection>
+          <ul>
+            {menu.map((item) => {
+              return (
+                <li onClick={() => router.push(item.url)}>
+                  <span>{item.label}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </AppBarSection>
+
+        <AppBarSpacer />
+
+        <AppBarSection className="actions">
+          <button className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base">
+            <BadgeContainer>
+              <span className="k-icon k-i-bell" />
+              <Badge
+                shape="dot"
+                themeColor="info"
+                size="small"
+                position="inside"
+              />
+            </BadgeContainer>
+          </button>
+        </AppBarSection>
+
+        <AppBarSection>
+          <span className="k-appbar-separator" />
+        </AppBarSection>
+
+        <AppBarSection>
+          <Avatar type="image">
+            <img src={kendokaAvatar} alt="KendoReact Layout Kendoka Avatar" />
+          </Avatar>
+        </AppBarSection>
+      </AppBar>
+      <style>{`
+          body {
+              background: #fffefe;
+              margin: 0;
+              padding: 0;
+          }
+          .title {
+              font-size: 18px;
+              margin: 0;
+          }
+          ul {
+              font-size: 14px;
+              list-style-type: none;
+              padding: 0;
+              margin: 0;
+              display: flex;
+          }
+          li {
+              margin: 0 10px;
+          }
+          li:hover {
+              cursor: pointer;
+              color: #84cef1;
+          }
+          .k-button k-button-md k-rounded-md k-button-solid k-button-solid-base {
+              padding: 0;
+          }
+          .k-badge-container {
+              margin-right: 8px;
+          }
+      `}</style>
+      <div>{children}</div>
+    </>
+  );
+}
